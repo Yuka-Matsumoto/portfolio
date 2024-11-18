d063cb5b0cab3ec8f7bd39fffb220b3667e05d14
松本友花 <yuka@matsumotoyukanonotobukkukonpyuta.local>
2024-11-18 10:12:32 +0900

    Migrate app to new repository

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/routes/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
